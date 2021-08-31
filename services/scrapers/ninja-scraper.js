5d6f5d7c4110ba19fee1d5a17927b83bbd546ec6
Magnus Färje <magnus.farje@gmail.com>
2021-08-31 15:27:03 +0200

    Refactored parser for ninja-summary-div

--- services/scrapers/ninja-scraper.js
+++ services/scrapers/ninja-scraper.js
@@ -3,11 +3,30 @@ const cheerio = require('cheerio');
 module.exports = class NinjaScraper {
     constructor(ninjaSummaryDiv) {
         this.ninjaSummaryDivHtml = cheerio.load(ninjaSummaryDiv);
-
-        this.name = this.#getName();
     };
 
-    #getName = () => {
+    get name() {
         return this.ninjaSummaryDivHtml("a > img").attr('alt') ?? '';
+    };
+
+    get imageUrl() {
+        return this.ninjaSummaryDivHtml("a > img").attr('src') ?? '';
+    };
+
+    get detailsPath() {
+        try {
+            return this.ninjaSummaryDivHtml('a').attr('href').split('/').pop() ?? '';
+        } catch {
+            return '';
+        };
+    };
+
+    get office() {
+        return this.ninjaSummaryDivHtml("h1 > a > span").text().split(' ')[1] ?? '';
+    };
+
+    get flag() {
+        return this.ninjaSummaryDivHtml("h1 > a > span").text().split(' ')[0] ?? '';
+
     };
 };
--- test/ninja-scraper-test.js
+++ test/ninja-scraper-test.js
@@ -3,11 +3,11 @@ const expect = require('chai').expect;
 const NinjaScraper = require('../services/scrapers/ninja-scraper');
 
 // The ninja-summary div contains the information we need for each employee which the scraper will be based on.
-const ninjaSummaryDiv = '<div class="ninja-summary" data-aos="flip-up" data-aos-offset="-20"> <div class="contact-info"> <h1><a href="meet/agron-kabashi">Agron Kabashi<span>🇸🇪 Lund</span></a></h1> <nav class="ninja-nav"> <a href="/meet/agron-kabashi" class="btn-secondary btn-small"><span>Get to know me</span></a> </nav> </div> <a href="/meet/agron-kabashi" data-bind="click: isActiveFilter(filters.grid) ? selectNinja : null"> <img class="portrait" src="https://i.1337co.de/profile/agron-kabashi-medium" alt="Agron Kabashi"> </a> </div>'
-const invalidDiv = '<div class="ninja-summary" data-aos="flip-up">test</div>'
+const ninjaSummaryDiv = '<div class="ninja-summary" data-aos="flip-up" data-aos-offset="-20"> <div class="contact-info"> <h1><a href="meet/agron-kabashi">Agron Kabashi<span>🇸🇪 Lund</span></a></h1> <nav class="ninja-nav"> <a href="/meet/agron-kabashi" class="btn-secondary btn-small"><span>Get to know me</span></a> </nav> </div> <a href="/meet/agron-kabashi" data-bind="click: isActiveFilter(filters.grid) ? selectNinja : null"> <img class="portrait" src="https://i.1337co.de/profile/agron-kabashi-medium" alt="Agron Kabashi"> </a> </div>';
+const invalidDiv = '<div class="ninja-summary" data-aos="flip-up">test</div>';
 
-const ninjaScraper = new NinjaScraper(ninjaSummaryDiv)
-const ninjaScraperWithInvalidData = new NinjaScraper(invalidDiv)
+const ninjaScraper = new NinjaScraper(ninjaSummaryDiv);
+const ninjaScraperWithInvalidData = new NinjaScraper(invalidDiv);
 
 describe('Ninja scraper', () => {
     describe('Get employee values from ninja-summary div', () => {
@@ -15,7 +15,31 @@ describe('Ninja scraper', () => {
             expect(ninjaScraper.name).to.equal('Agron Kabashi');
         });
         it('invalid data should return empty name', () => {
-            expect(ninjaScraperWithInvalidData.name).to.equal('')
+            expect(ninjaScraperWithInvalidData.name).to.equal('');
+        });
+        it('should return image URL', () => {
+            expect(ninjaScraper.imageUrl).to.equal('https://i.1337co.de/profile/agron-kabashi-medium');
+        });
+        it('invalid data should return empty image URL', () => {
+            expect(ninjaScraperWithInvalidData.imageUrl).to.equal('');
+        });
+        it('should return details path', () => {
+            expect(ninjaScraper.detailsPath).to.equal('agron-kabashi');
+        });
+        it('invalid data should return empty details path', () => {
+            expect(ninjaScraperWithInvalidData.detailsPath).to.equal('');
+        });
+        it('should return office', () => {
+            expect(ninjaScraper.office).to.equal('Lund');
+        });
+        it('invalid data should return empty office', () => {
+            expect(ninjaScraperWithInvalidData.office).to.equal('');
+        });
+        it('should return flag', () => {
+            expect(ninjaScraper.flag).to.equal('🇸🇪');
+        });
+        it('invalid data should return empty flag', () => {
+            expect(ninjaScraperWithInvalidData.flag).to.equal('');
         });
     });
 });
